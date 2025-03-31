cbffb4ab30289dcd20a216dae53d6fa6e8511fee
Gustavo <gustavinho_ulp@hotmail.com>
2025-03-31 19:53:50 -0300

    exercício 01
    Crie um programa que peça para o usuário digitar 2 números(prompt), some esses números e mostre o resultado em um alert

---
+++ exercicio-01/ex01.js
@@ -0,0 +1,4 @@
+let numeros = prompt ("Digite um numero para somar")
+let numeros2 = prompt ("Digite um numero para somar")
+let resultado = parseFloat(numeros)+ parseFloat(numeros2)
+alert ("O resultado é: " + resultado)
