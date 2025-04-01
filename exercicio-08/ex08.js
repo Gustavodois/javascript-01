31a0e320a4d1ed1fa6873a095311d61e064a59d6
Gustavo <gustavinho_ulp@hotmail.com>
2025-03-31 21:48:54 -0300

    Exercício 08
    Faça um algoritmo que leia 5 números do teclado, e faça uma média.

---
+++ exercicio-08/ex08.js
@@ -0,0 +1,9 @@
+let num1 = parseFloat(prompt("Digite um numero para mostrar a media deles."))
+let num2 = parseFloat(prompt("Digite um numero para mostrar a media deles."))
+let num3 = parseFloat(prompt("Digite um numero para mostrar a media deles."))
+let num4 = parseFloat(prompt("Digite um numero para mostrar a media deles."))
+let num5 = parseFloat(prompt("Digite um numero para mostrar a media deles."))
+
+let resultado = (num1 + num2 + num3 + num4 + num5) / 5
+
+alert ("O resultado da média dos numeros digitados foi: " + resultado)
