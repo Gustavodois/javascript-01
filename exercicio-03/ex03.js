2f8c401ff8c8d5b0459a578fca7e9920dfb27756
Gustavo <gustavinho_ulp@hotmail.com>
2025-03-31 21:13:49 -0300

    Exercício 03
    Solicite ao usuário o nome e a idade (prompt).
            Se a pessoa tiver menos de 18 anos exiba a mensagem "Olá [nome] você ainda não pode fazer aulas de direção"
            Se a pessoa tiver 18 anos ou mais exiba a mensagem "Olá [nome], seja bem vindo(a) a nossa escola! Já fez sua matrícula?

---
+++ exercicio-03/ex03.js
@@ -0,0 +1,9 @@
+let nome = prompt ("Digite seu Nome")
+let idade = prompt ("Digite sua Idade")
+
+if (idade < 18){
+    alert ("Olá " + nome + " você ainda não pode fazer aulas de direção")
+}
+else{
+    alert ("Olá " + nome + " , seja bem vindo(a) a nossa escola! Já fez sua matrícula?")
+}
